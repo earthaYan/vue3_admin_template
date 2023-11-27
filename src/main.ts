5da9960ffe2677754876ec16a8e71722a083f1b0
yueyueyan <yueyueyan1215@163.com>
2023-11-28 00:03:50 +0800

    chore: config element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(ElementPlus, {
+    // 国际化配置
+    locale: zhCn,
+  })
+  .mount('#app')
