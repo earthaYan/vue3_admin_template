cea35c2172afc297e1bb9ea992041e73bd3160ec
yueyueyan <yueyueyan1215@163.com>
2023-11-28 18:57:24 +0800

    chore: add config for src alias

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { ElButton } from 'element-plus'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 <template>
   <div>
     <el-button type="danger">按钮</el-button>
+    <HelloWorld msg="hello" />
   </div>
 </template>
 
