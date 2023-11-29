18ad7282bd6ab3d5d27298d5bbdccc56ca8a4e22
yueyueyan <yueyueyan1215@163.com>
2023-11-29 20:44:19 +0800

    chore: config scss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from '@/App.vue'
+import '@/styles/index.scss'
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // 引入自定义插件对象：注册整个项目的全局组件
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { UserConfig, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
@@ -7,6 +7,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve('./src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      // 配置对应的scss变量可全局使用
+      scss: {
+        javascriptEnabled: true,
+        additionalData: '@import "./src/styles/variable.scss";',
+      },
     },
   },
 })
